41cfdce59df2a26b8778f13e5db13ed40851db3e
Vasiliy Stepanov <xarek_xxx@yahoo.com>
2024-09-06 13:47:31 +0300

    верстка фронта

--- main.go
+++ main.go
@@ -6,42 +6,49 @@ import (
 	"net/http"
 )
 
-type Adrress struct {
-	Street string
-	House  int
-	Name   string
-}
-
 type EventViewData struct {
-	Title       string
-	Description string
-	Date        string
-	Adrress
+	Title         string
+	Description   string
+	EventDate     string
+	EventTypeName string
+	Address       string
+	Period        string
+	ChatId        string
 }
 
 func main() {
-
 	fs := http.FileServer(http.Dir("static"))
 	http.Handle("/", fs)
-
 	http.HandleFunc("/event", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == "GET" {
 			title := r.URL.Query().Get("title")
 			msg := r.URL.Query().Get("msg")
+			eventTypeName := r.URL.Query().Get("eventTypeName")
+			address := r.URL.Query().Get("address")
+			period := r.URL.Query().Get("period")
+			date := r.URL.Query().Get("date")
+			chatId := r.URL.Query().Get("chatId")
+
 			data := EventViewData{
-				Title:       title,
-				Description: msg,
+				Title:         title,
+				Description:   msg,
+				EventTypeName: eventTypeName,
+				Address:       address,
+				Period:        period,
+				EventDate:     date,
+				ChatId:        chatId,
 			}
 			tmpl, _ := template.ParseFiles("static/templates/index.html")
 			tmpl.Execute(w, data)
 		}
 
 		if r.Method == "POST" {
-			answer := r.PostForm.Get("answer")
+			name := r.PostForm.Get("name")
 			// todo: обработать согласие
-			fmt.Println(answer)
+			fmt.Println(name)
 			//response := fmt.Sprintf("Product category=%s id=%s", cat, id)
-			fmt.Fprint(w, `{"data":"responce"}`)
+			response := fmt.Sprintf(`{"data":"ok","name": "%n" }`, name)
+			fmt.Fprint(w, response)
 		}
 	})
 
--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,11 +1,27 @@
+function register(event) {
+  const formElement = document.getElementById("registrationForm");
+  const formData = new FormData(formElement);
+  const registerData = Object.fromEntries(formData);
+  console.log(registerData);
+  fetch("event", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(registerData), // Ваши данные для отправки
+  })
+    .then((res) => res.json())
+    .then((data) => console.log("Успешно:", data))
+    .catch((error) => console.error("Ошибка:", error));
+  debugger;
+}
+
 function solution(answer) {
-    console.log(answer)    
-    fetch('event', {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({"answer": answer}) // Ваши данные для отправки
-      })
-      .then(res => res.json())
-      .then(data => console.log('Успешно:', data))
-      .catch(error => console.error('Ошибка:', error));
+  console.log(answer);
+  fetch("event", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ answer: answer }), // Ваши данные для отправки
+  })
+    .then((res) => res.json())
+    .then((data) => console.log("Успешно:", data))
+    .catch((error) => console.error("Ошибка:", error));
 }
